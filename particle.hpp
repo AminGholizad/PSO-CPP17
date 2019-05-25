02703f137528679e089b469425b5378f03218a4f
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-25 14:38:26 +0430

    Update particle.hpp

--- particle.hpp
+++ particle.hpp
@@ -1,4 +1,15 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
-
+class Particle {
+  public:
+    inline Particle() = default;
+    inline Particle(Particle const&) = default;
+    inline Particle(Particle&&) = default;
+    inline Particle& operator=(Particle const&) = default;
+    inline Particle& operator=(Particle&&) = default;
+    inline Particle (/*arguments*/) {/*body*/};
+  private:
+    std::vector<double> x;
+    std::vector<double> v;
+};
 #endif //PARTICLE_H
