32b8b78e8003ce5d87e8fb64a27924d95dda1b0d
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-25 22:43:02 +0430

    Update rand.cpp

--- rand.cpp
+++ rand.cpp
@@ -1,9 +1,27 @@
 #include "rand.hpp"
-
-template <typename T>
-T rand::randin(T,T){
-
+#include <random>
+inline std::mt19937& rand::Generator(){
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  return gen;
 }
-double rand::rand(){
-
+template <typename T>
+inline T rand::unifrand(T a,T b){
+  if constexpr(std::is_integral_v<T>){
+    std::uniform_int_distribution<T> dis(a, b);
+    return dis(Generator());
+  }
+  else {
+    std::uniform_real_distribution<T> dis(a, b);
+    return dis(Generator());
+  }
+}
+inline double rand::rand(){
+  return unifrand(0.,1.);
+}
+template<typename Iter, typename RandomGenerator>
+inline Iter rand::select_randomly(Iter start, Iter end, RandomGenerator& g=Generator()){
+    std::uniform_int_distribution<int> dis(0, std::distance(start, end) - 1);
+    std::advance(start, dis(g));
+    return start;
 }
