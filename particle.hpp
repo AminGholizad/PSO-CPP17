63c0b7c391adf5c6416a01dfacdb5732e3a3c06a
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-25 20:52:00 +0430

    Update particle.hpp

--- particle.hpp
+++ particle.hpp
@@ -1,5 +1,7 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
+using vec=std::vector<double>;
+using prob=std::function<std::pair<double,double>(vec)>;
 class Particle {
   public:
     inline Particle() = default;
@@ -7,9 +9,21 @@ class Particle {
     inline Particle(Particle&&) = default;
     inline Particle& operator=(Particle const&) = default;
     inline Particle& operator=(Particle&&) = default;
-    inline Particle (/*arguments*/) {/*body*/};
+    inline Particle (vec l,vec u,prob problem);
+    inline update(double w,double[] c,double pm,Particle gBest,prob problem);
+    inline updateV(double w,double[] c,Particle gBest);
+    inline updateX();
+    inline updatePBest();
+    inline Mutate(double pm,prob problem);
   private:
-    std::vector<double> x;
-    std::vector<double> v;
+    vec l;
+    vec u;
+    vec x;
+    vec v;
+    double cost;
+    double infeasablity;
+    vec pBest;
+    double pBest_cost;
+    double pBest_infeasablity;
 };
 #endif //PARTICLE_H
