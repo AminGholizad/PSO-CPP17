fcebac2140bdb158af1fad90de4e6acfae442520
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-25 13:54:09 +0430

    Update pso.hpp

--- pso.hpp
+++ pso.hpp
@@ -1,4 +1,14 @@
 #ifndef PSO_H
 #define PSO_H
-
+class PSO { 
+  public:
+    inline PSO() = default;
+    inline PSO(PSO const&) = default;
+    inline PSO(PSO&&) = default;
+    inline PSO& operator=(PSO const&) = default;
+    inline PSO& operator=(PSO&&) = default;
+    inline PSO (/*arguments*/) {/*body*/};
+  private:
+    /* code */
+};
 #endif //PSO_H
