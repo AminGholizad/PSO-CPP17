fea43a61aca07a85789e4c3c1c171b17c642d30b
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-25 23:39:21 +0430

    Update rand.cpp

--- rand.cpp
+++ rand.cpp
@@ -1,12 +1,10 @@
 #include "rand.hpp"
-#include <random>
-inline std::mt19937& rand::Generator(){
+inline std::mt19937& rnd::Generator(){
   static std::random_device rd;
   static std::mt19937 gen(rd());
   return gen;
 }
-template <typename T>
-inline T rand::unifrnd(T a,T b){
+template <typename T> inline T Rand::unifrnd(T a,T b){
   if constexpr(std::is_integral_v<T>){
     std::uniform_int_distribution<T> dis(a, b);
     return dis(Generator());
@@ -16,12 +14,16 @@ inline T rand::unifrnd(T a,T b){
     return dis(Generator());
   }
 }
-inline double rand::rand(){
-  return unifrand(0.,1.);
+inline double rnd::rand(){
+  return unifrnd(0.,1.);
 }
 template<typename Iter, typename RandomGenerator>
-inline Iter rand::select_randomly(Iter start, Iter end, RandomGenerator& g=Generator()){
+inline Iter rnd::select_randomly(Iter start, Iter end, RandomGenerator& g){
     std::uniform_int_distribution<int> dis(0, std::distance(start, end) - 1);
     std::advance(start, dis(g));
     return start;
+}
+template<typename Iter>
+inline Iter rnd::select_randomly(Iter start, Iter end){
+  return Rand::select_randomly(start,end,Generator());
 }
