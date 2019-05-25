db4d5efd9b78240c9b72197242272b37f6f6a66a
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-25 23:39:36 +0430

    Update rand.hpp

--- rand.hpp
+++ rand.hpp
@@ -1,10 +1,13 @@
 #ifndef RAND
 #define RAND
-namespace rand{
-std::mt19937& Generator();
-template <typename T> T unifrnd(T,T);
-double rand();
-template<typename Iter, typename RandomGenerator>
-Iter select_randomly(Iter, Iter, RandomGenerator&)
+#include <random>
+#include <algorithm>
+namespace rnd{
+  std::mt19937& Generator();
+  template <typename T> T unifrnd(T,T);
+  double rand();
+  template<typename Iter, typename RandomGenerator>
+  Iter select_randomly(Iter, Iter, RandomGenerator&);
+  template<typename Iter> Iter select_randomly(Iter, Iter);
 }
 #endif
