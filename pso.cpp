ea2f8d304d41c8d56c2f1d391abcf061ed900ddb
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-26 13:27:49 +0430

    Update pso.cpp

--- pso.cpp
+++ pso.cpp
@@ -1,13 +1,13 @@
-#include "particle.hpp"
 Particle pso(double c[],double iw[],size_t max_iter,vec lower_bound,vec upper_bound,size_t swarm_size,double mu,prob& problem){
   auto w = [&](size_t it){ return ((max_iter - it) - (iw[0] - iw[1]))/max_iter + iw[1];};
   auto pm = [&](size_t it){ return pow(1-(it-1)/(max_iter-1),(1/mu));};
   std::vector<Particle> swarm;
   for (size_t i = 0; i < swarm_size; i++)
     swarm.emplace_back(lower_bound,upper_bound,problem);
-  Particle gBest;
+  auto gBest = swarm[0];
   for (size_t i = 0; i < max_iter; i++) {
-    gBest = Particle::get_Best(swarm);
+    if (auto tmp = Particle::get_Best(swarm); tmp.dominates(gBest))
+      gBest=tmp;
     auto wc = w(i);
     auto pc = pm(i);
     for (size_t j = 0; j < swarm_size; j++) {
