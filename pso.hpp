98e69105cbb5e24bbed4bc93d7c3b0e2bf01c18a
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-26 16:53:44 +0430

    Update pso.hpp

--- pso.hpp
+++ pso.hpp
@@ -1,14 +1,15 @@
 #ifndef PSO_H
 #define PSO_H
 #include <iostream>
+#include <cmath>
 #include "particle.hpp"
 namespace pso {
-  template<ull N>
-  Particle<N> pso(const double c[2],const double iw[2],const size_t max_iter,const arr<N>& lower_bound,const arr<N>& upper_bound,const size_t swarm_size,const double mu,const prob<N>& problem){
+  template<ull N,ull S>
+  Particle<N> pso(const arr<N>& lower_bound,const arr<N>& upper_bound,const prob<N>& problem,const size_t max_iter,const size_t swarm_size,const double c[2],const double iw[2],const double mu=0.1){
     auto w = [&](size_t it){ return ((max_iter - it) - (iw[0] - iw[1]))/max_iter + iw[1];};
-    auto pm = [&](size_t it){ return pow(1-it/(max_iter-1.),1/mu);};
-    std::vector<Particle<N>> swarm(swarm_size);
-    for (size_t i = 0; i < swarm_size; i++)
+    auto pm = [&](size_t it){ return std::pow(1-it/(max_iter-1.),1/mu);};
+    std::array<Particle<N>,S> swarm;
+    for (size_t i = 0; i < swarm.size(); i++)
       swarm[i]=Particle<N>(lower_bound,upper_bound,problem);
     auto gBest = swarm[0];
     for (size_t i = 0; i < max_iter; i++) {
@@ -16,7 +17,7 @@ namespace pso {
         gBest=tmp;
       auto wc = w(i);
       auto pc = pm(i);
-      for (size_t j = 0; j < swarm_size; j++) {
+      for (size_t j = 0; j < swarm.size(); j++) {
         swarm[j].update(wc,c,pc,gBest,problem);
       }
     }
