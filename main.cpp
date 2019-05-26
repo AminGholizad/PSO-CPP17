da08b405e40422c52521955438c599ebc47e88eb
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-26 17:38:29 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "pso.hpp"
 template<pso::ull N>
-std::pair<double,double>cost_fcn(pso::arr<N> x){
+std::pair<double,double>cost_fcn(pso::vars<N> x){
   double s = 0.;
   double c = 0.;
   for (size_t i = 0; i < x.size(); i++) {
@@ -13,8 +13,8 @@ std::pair<double,double>cost_fcn(pso::arr<N> x){
 int main(){
   const pso::ull Nvars = 4;
   const pso::ull Swarm_size = 100;
-  pso::arr<Nvars> l{-10.,-10.,-10.,-10.};
-  pso::arr<Nvars> u{10.,10.,10.,10.};
+  pso::vars<Nvars> l{-10.,-10.,-10.,-10.};
+  pso::vars<Nvars> u{10.,10.,10.,10.};
   auto p = pso::pso<Nvars,Swarm_size>(l,u,cost_fcn<Nvars>);
   p.info();
   return 0;
