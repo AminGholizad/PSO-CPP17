f7ef74aa59016f20c3fd99427bf5f69f59f21c24
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-26 13:14:22 +0430

    Update particle.hpp

--- particle.hpp
+++ particle.hpp
@@ -14,7 +14,7 @@ class Particle {
     inline Particle(Particle&&) = default;
     inline Particle& operator=(Particle const&) = default;
     inline Particle& operator=(Particle&&) = default;
-    inline Particle(vec,vec,prob&):l{l},u{u} {
+    inline Particle(vec l,vec u,const prob& problem):l{l},u{u} {
       for (size_t i = 0; i < l.size(); i++) x[i]=rnd::unifrnd(l[i],u[i]);
       std::fill(v.begin(),v.begin()+x.size(),0);
       std::tie(cost,infeasablity)=problem(x);
@@ -22,14 +22,14 @@ class Particle {
       pBest_cost=cost;
       pBest_infeasablity=infeasablity;
     }
-    inline void update(double,double[],double,Particle,prob&){
+    inline void update(const double w,const double c[],const double pm,const Particle& gBest,const prob& problem){
       updateV(w,c,gBest);
       updateX();
       std::tie(cost,infeasablity) = problem(x);
       Mutate(pm,problem);
       updatePBest();
     }
-    inline void updateV(double,double[],Particle){
+    inline void updateV(const double w,const double c[],const Particle& gBest){
       for (size_t i = 0; i < v.size(); i++) {
         v[i]=w*v[i]+c[0]*rnd::rand()*(pBest[i]-x[i])+c[1]*rnd::rand()*(gBest.x[i]-x[i]);
       }
@@ -37,7 +37,10 @@ class Particle {
     inline void updateX(){
       for (size_t i = 0; i < x.size(); i++) {
         x[i]+=v[i];
-        if (x[i]>u[i] || x[i]<l[i]) x[i]-=2*v[i];
+        if (x[i]>u[i] || x[i]<l[i]){
+          v[i]*=-1;
+          x[i]+=2*v[i];
+        }
       }
     }
     inline void updatePBest(){
@@ -47,31 +50,31 @@ class Particle {
         pBest_infeasablity=infeasablity;
       }
     }
-    inline void Mutate(double,prob&){
+    inline void Mutate(const double pm,const prob& problem){
       if (rnd::rand()>pm) return;
       int j = rnd::unifrnd((size_t)0,x.size());
       double dx=pm*(u[j]-l[j]);
       double lb=std::max(x[j]-dx,l[j]);
       double ub=std::min(x[j]+dx,u[j]);
       auto X = x;
-      X[j]=rnd::unifrnd(lb,ub);//todo
+      X[j]=rnd::unifrnd(lb,ub);
       auto [c,i] = problem(X);
       if (i < infeasablity && c < cost){
-        x=X;
+        x[j]=X[j];
         cost = c;
         infeasablity = i;
       }
       else if (rnd::rand()<0.5){
-        x=X;
+        x[j]=X[j];
         cost = c;
         infeasablity = i;
       }
     }
-    inline static Particle get_gBest(const std::vector<Particle>&){
+    inline static Particle get_gBest(const std::vector<Particle>& swarm){
       return *std::min_element(swarm.begin(),swarm.end(),
-                              [](auto a,auto b){
-                                return ((a->infeasablity<=b->infeasablity) && (a->cost < b->cost));
-                              })
+                              [](const auto& a,const auto& b){
+                                return ((a.infeasablity<=b.infeasablity) && (a.cost < b.cost));
+                              });
     }
   private:
     vec l;
