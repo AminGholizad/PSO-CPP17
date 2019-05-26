6840fcacea51251eecf22b504d6b718d11867183
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-26 17:36:59 +0430

    Update pso.hpp

--- pso.hpp
+++ pso.hpp
@@ -4,7 +4,7 @@
 #include "particle.hpp"
 namespace pso {
   template<ull N,ull S>
-  Particle<N> pso(const arr<N>& lower_bound,const arr<N>& upper_bound,const prob<N>& problem,const size_t max_iter=1000,const std::array<double,2> c={0.2,0.2},const std::array<double,2> iw={0.1,0.01},const double mu=0.1){
+  Particle<N> pso(const arr<N>& lower_bound,const arr<N>& upper_bound,const Problem<N>& problem,const size_t max_iter=1000,const std::array<double,2>& c={0.2,0.2},const std::array<double,2>& iw={0.1,0.01},const double mu=0.1){
     auto w = [&](size_t it){ return ((max_iter - it) - (iw[0] - iw[1]))/max_iter + iw[1];};
     auto pm = [&](size_t it){ return std::pow(1-it/(max_iter-1.),1/mu);};
     std::array<Particle<N>,S> swarm;
@@ -17,7 +17,7 @@ namespace pso {
       auto wc = w(i);
       auto pc = pm(i);
       for (size_t j = 0; j < swarm.size(); j++) {
-        swarm[j].update(wc,c,pc,gBest,problem);
+        swarm[j].update(gBest,problem,wc,c,pc);
       }
     }
     return gBest;
