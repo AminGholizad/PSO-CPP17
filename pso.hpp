a4f667276ccff74e53c25614b32f9d33c686f9a7
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-26 13:27:52 +0430

    Update pso.hpp

--- pso.hpp
+++ pso.hpp
@@ -1,5 +1,5 @@
 #ifndef PSO_H
 #define PSO_H
-
-};
+#include "particle.hpp"
+Particle pso(double c[],double iw[],size_t max_iter,vec lower_bound,vec upper_bound,size_t swarm_size,double mu,prob& problem);
 #endif //PSO_H
