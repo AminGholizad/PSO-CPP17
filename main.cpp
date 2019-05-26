bc8277e11b86c0cc3b288ce33f2774f4a8a9db6c
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-26 16:32:15 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "pso.hpp"
-template<ull N>
-std::pair<double,double>cost_fcn(arr<N> x){
+template<pso::ull N>
+std::pair<double,double>cost_fcn(pso::arr<N> x){
   double s = 0.;
   double c = 0.;
   for (size_t i = 0; i < x.size(); i++) {
@@ -13,10 +13,10 @@ std::pair<double,double>cost_fcn(arr<N> x){
 int main(){
   double c[2] = {0.2,0.2};
   double w[2] = {0.1,0.01};
-  const ull N = 3;
-  arr<N> l{-10.,-10.,-10.};
-  arr<N> u{10.,10.,10.};
-  auto p = pso<N>(c,w,500,l,u,100,0.1,cost_fcn<N>);
+  const pso::ull N = 4;
+  pso::arr<N> l{-10.,-10.,-10.,-10.};
+  pso::arr<N> u{10.,10.,10.,10.};
+  auto p = pso::pso<N>(c,w,500,l,u,100,0.1,cost_fcn<N>);
   p.info();
   return 0;
 }
