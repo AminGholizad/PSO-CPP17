b6b2061bb9dbf6222e2e1dcd152dc511536d8a6e
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-26 14:26:30 +0430

    Update pso.hpp

--- pso.hpp
+++ pso.hpp
@@ -1,12 +1,13 @@
 #ifndef PSO_H
 #define PSO_H
+#include <iostream>
 #include "particle.hpp"
-Particle pso(double c[2],double iw[2],size_t max_iter,vec lower_bound,vec upper_bound,size_t swarm_size,double mu,const prob& problem){
+Particle pso(const double c[2],const double iw[2],const size_t max_iter,const vec& lower_bound,const vec& upper_bound,const size_t swarm_size,const double mu,const prob& problem){
   auto w = [&](size_t it){ return ((max_iter - it) - (iw[0] - iw[1]))/max_iter + iw[1];};
   auto pm = [&](size_t it){ return pow(1-(it-1)/(max_iter-1),(1/mu));};
   std::vector<Particle> swarm;
   for (size_t i = 0; i < swarm_size; i++)
-    swarm.emplace_back(lower_bound,upper_bound,problem);
+    swarm.push_back(Particle(lower_bound,upper_bound,problem));
   auto gBest = swarm[0];
   for (size_t i = 0; i < max_iter; i++) {
     if (auto tmp = Particle::get_Best(swarm); tmp.dominates(gBest))
@@ -17,6 +18,7 @@ Particle pso(double c[2],double iw[2],size_t max_iter,vec lower_bound,vec upper_
       swarm[j].update(wc,c,pc,gBest,problem);
     }
   }
+  std::cout << "pso" << '\n';
   return gBest;
 }
 #endif //PSO_H
