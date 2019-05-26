b83e893b4aa767c1c11e9a5fdfc4e0bbab1a4e89
amin-gholizad <39440488+amin-gholizad@users.noreply.github.com>
2019-05-26 16:24:32 +0430

    Update pso.hpp

--- pso.hpp
+++ pso.hpp
@@ -2,15 +2,16 @@
 #define PSO_H
 #include <iostream>
 #include "particle.hpp"
-Particle pso(const double c[2],const double iw[2],const size_t max_iter,const vec& lower_bound,const vec& upper_bound,const size_t swarm_size,const double mu,const prob& problem){
+template<ull N>
+Particle<N> pso(const double c[2],const double iw[2],const size_t max_iter,const vec<N> & lower_bound,const vec<N> & upper_bound,const size_t swarm_size,const double mu,const prob<N>& problem){
   auto w = [&](size_t it){ return ((max_iter - it) - (iw[0] - iw[1]))/max_iter + iw[1];};
   auto pm = [&](size_t it){ return pow(1-it/(max_iter-1.),1/mu);};
-  std::vector<Particle> swarm;
+  std::vector<Particle<N>> swarm(swarm_size);
   for (size_t i = 0; i < swarm_size; i++)
-    swarm.push_back(Particle(lower_bound,upper_bound,problem));
+    swarm[i]=Particle<N>(lower_bound,upper_bound,problem);
   auto gBest = swarm[0];
   for (size_t i = 0; i < max_iter; i++) {
-    if (auto tmp = Particle::get_Best(swarm); tmp.dominates(gBest))
+    if (auto tmp = Particle<N>::get_Best(swarm); tmp.dominates(gBest))
       gBest=tmp;
     auto wc = w(i);
     auto pc = pm(i);
